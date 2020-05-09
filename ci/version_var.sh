40ae64304211080238f53e3c62292c404e2ac6f8
Troy Ronda <troy.ronda@securekey.com>
2020-05-09 18:55:13 -0400

    ci: publish snapshots to external repo
    Signed-off-by: Troy Ronda <troy.ronda@securekey.com>

--- ci/version_var.sh
+++ ci/version_var.sh
@@ -10,12 +10,21 @@
 BASE_VERSION=0.1.4
 IS_RELEASE=false
 
-if [ $IS_RELEASE == false ]
+# Project Parameters
+SOURCE_REPO=edv
+BASE_PKG_NAME=edv-rest
+RELEASE_REPO=docker.pkg.github.com/trustbloc/${SOURCE_REPO}
+SNAPSHOT_REPO=docker.pkg.github.com/trustbloc-cicd/snapshot
+
+if [ ${IS_RELEASE} = false ]
 then
   EXTRA_VERSION=snapshot-$(git rev-parse --short=7 HEAD)
-  PROJECT_VERSION=$BASE_VERSION-$EXTRA_VERSION
+  PROJECT_VERSION=${BASE_VERSION}-${EXTRA_VERSION}
+  PROJECT_PKG_REPO=${SNAPSHOT_REPO}
 else
-  PROJECT_VERSION=$BASE_VERSION
+  PROJECT_VERSION=${BASE_VERSION}
+  PROJECT_PKG_REPO=${RELEASE_REPO}
 fi
 
 export EDV_REST_TAG=$PROJECT_VERSION
+export EDV_REST_PKG=${PROJECT_PKG_REPO}/${BASE_PKG_NAME}
